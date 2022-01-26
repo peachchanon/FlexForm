56bbbb90c8832e26cd4335a2a129918c2de7355b
chanonpeach <chanonpanarong@live.com>
2022-01-26 17:26:45 +0700

    Feat: dev

--- flexform/src/App.vue
+++ flexform/src/App.vue
@@ -9,24 +9,14 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Prompt:wght@300;500;600&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'Prompt', sans-serif;
+  height: 100%;
+  margin: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html, body {
+  height: 100%;
+  margin: 0;
 }
 </style>
