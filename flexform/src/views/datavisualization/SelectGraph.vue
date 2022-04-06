d64a2fe356df4b27551a48ac1e8f70377198de9c
b-apichaya1999 <b-apichaya1999>
2022-04-06 16:52:46 +0700

    Feat: with progress bar

--- flexform/src/views/datavisualization/SelectGraph.vue
+++ flexform/src/views/datavisualization/SelectGraph.vue
@@ -7,6 +7,22 @@
         <Icon class="semibold24 icon blue10 tw-mr-1.5" icon="heroicons-outline:chart-pie"/>
         <span class="semibold24 blue10 tw-pl-1">Data Visualization</span>
       </div>
+      <div class="step-wizard">
+        <ul class="step-wizard-list">
+          <li class="step-wizard-item">
+            <span class="progress-count">1</span>
+            <span class="progress-label">List</span>
+          </li>
+          <li class="step-wizard-item current-item">
+            <span class="progress-count">2</span>
+            <span class="progress-label">Build</span>
+          </li>
+          <li class="step-wizard-item">
+            <span class="progress-count">3</span>
+            <span class="progress-label">Export</span>
+          </li>
+        </ul>
+      </div>
       <div class="box2 tw-flex tw-flex-row tw-justify-center" style="min-height: 100%; ">
         <div class="cardleft bg-red5 base-shadow tw-mt-5 tw-w-1/5 ">
           <div>Hello ja</div>
@@ -66,5 +82,103 @@ export default {
 }
 .cardright{
   border-top-right-radius:$radius10px;
+}
+.step-wizard {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
+.step-wizard-list{
+  color: $blue10;
+  list-style-type: none;
+  border-radius: 10px;
+  display: flex;
+  padding: 20px 10px;
+  position: relative;
+  z-index: 10;
+}
+
+.step-wizard-item{
+  padding: 0 20px;
+  flex-basis: 0;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  min-width: 170px;
+  position: relative;
+}
+.step-wizard-item + .step-wizard-item:after{
+  content: "";
+  clear: both;
+  position: absolute;
+  left: 0;
+  top: 19px;
+  background: $blue5;
+  width: 100%;
+  height: 3px;
+  transform: translateX(-50%);
+  z-index: -10;
+}
+.progress-count{
+  height: 40px;
+  width:40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  font-weight: 600;
+  margin: 0 auto;
+  position: relative;
+  z-index:10;
+  color: transparent;
+}
+.progress-count:after{
+  content: "";
+  height: 40px;
+  width: 40px;
+  background: $blue5;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  z-index: -10;
+}
+.progress-count:before{
+  content: "";
+  height: 10px;
+  width: 20px;
+  border-left: 3px solid #fff;
+  border-bottom: 3px solid #fff;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -60%) rotate(-45deg);
+  transform-origin: center center;
+}
+.progress-label{
+  font-size: 14px;
+  font-weight: 600;
+  margin-top: 10px;
+}
+.current-item .progress-count:before,
+.current-item ~ .step-wizard-item .progress-count:before{
+  display: none;
+}
+.current-item ~ .step-wizard-item .progress-count:after{
+  height:10px;
+  width:10px;
+}
+.current-item ~ .step-wizard-item .progress-label{
+  opacity: 0.5;
+}
+.current-item .progress-count:after{
+  background: #fff;
+  border: 2px solid $blue5;
+}
+.current-item .progress-count{
+  color: $blue5;
 }
 </style>
