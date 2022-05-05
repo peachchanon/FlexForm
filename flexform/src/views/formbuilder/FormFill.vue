2cd54f51ae4a76d0e4fc47357d6a9fa3c42a423d
tudchalee <tudchapan.lee@mail.kmutt.ac.th>
2022-05-05 11:52:22 +0700

    feat: form form builder form fill save modal and block route

--- flexform/src/views/formbuilder/FormFill.vue
+++ flexform/src/views/formbuilder/FormFill.vue
@@ -3,7 +3,7 @@
     <!-- Header -->
     <div class="tw-w-full bg-white base-padding base-shadow tw-fixed tw-z-50">
       <div class="tw-flex tw-flex-row tw-items-center">
-        <div class="tw-mr-4">
+        <div class="tw-mr-4" @click="doShowSaveModal">
           <div class="button__circle">
             <Icon class="icon__style__large blue10" icon="heroicons-outline:chevron-left"/>
           </div>
@@ -125,6 +125,31 @@
       <span class="medium14 white">FlexForm</span>
       <span class="medium14 white">Create by {{FormStructure.CreatedByUser}}</span>
     </div>
+    <!-- Continue Fill Form Modal -->
+    <transition name="theme-modal-fade" v-if="StateSaveModal">
+      <div class="theme-modal-backdrop">
+        <div class="theme-modal">
+          <header class="base-padding tw-flex tw-flex-row tw-items-center tw-justify-start tw-relative">
+            <Icon class="icon__style__large tw-mr-2 red5" icon="heroicons-outline:download"/>
+            <span class="semibold24 red5">Continue fill form ?</span>
+          </header>
+          <section class="tw-pl-3 tw-pr-3 tw-py-4 tw-overflow-x-hidden">
+            <span class="medium16 grey10">Do you want to finish filling the form before exit? <br>If you don’t submit, your changes will be lost.</span>
+          </section>
+          <footer class="tw-p-2.5 tw-flex tw-flex-row tw-justify-end">
+<!--            <div class="tw-w-1/3 tw-mr-1">-->
+<!--              <div class="button__style__white red" @click="doShowSaveModal">Cancel</div>-->
+<!--            </div>-->
+            <div class="tw-w-1/3 tw-mr-1">
+              <div class="button__style__white red" @click="PrevPage">No</div>
+            </div>
+            <div class="tw-w-1/3 tw-ml-1">
+              <div class="button__style__red" @click="doActionButton">Fill Form</div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -161,6 +186,8 @@ export default {
   },
   data() {
     return {
+      // Save
+      StateSaveModal: false,
       // State Sections and Components
       StateSelectSectionIndex: 0,
       // Form Structure
@@ -238,53 +265,69 @@ export default {
     },
   },
   mounted() {
-    axios.get('http://localhost:4000/api/Flexform/' + this.ClickedForm)
-        .then(response => {
-          if(response.status===200 && response.data) {
-            this.FormData = response.data
-            console.log(this.ClickedForm)
-            console.log(this.FormData)
+    console.log(this.ClickedForm)
+    if(this.ClickedForm !== undefined){
+      axios.get('http://localhost:4000/api/Flexform/' + this.ClickedForm)
+          .then(response => {
+            if (response.status === 200 && response.data) {
+              this.FormData = response.data
+              console.log(this.ClickedForm)
+              console.log(this.FormData)
 
-            this.FormStructure = this.CapitalObj(this.FormData)
-            console.log(this.FormStructure)
-            
-            this.FormInput.FormId = this.FormStructure.FormId
-            this.FormInput.InputByUser= ''
-            this.FormInput.Timestamp= this.timestamp
-            this.FormInput.Sections= []
-            this.FormStructure.Sections.forEach(
-                (elementSection, indexSection)=>{
-                  this.FormInput.Sections.push(
-                      {
-                        SectionId: this.FormStructure.Sections[indexSection].SectionId,
-                        Components: []
-                      }
-                  )
-                  this.FormStructure.Sections[indexSection].Components.forEach(
-                      (elementComponent, indexComponent)=>{
-                        if(
-                            elementComponent.ComponentType !== 'heading'
-                            && elementComponent.ComponentType !== 'paragraph'
-                        ){
-                          this.FormInput.Sections[indexSection].Components.push(
-                              {
-                                ComponentId: this.FormStructure.Sections[indexSection].Components[indexComponent].ComponentId,
-                                ComponentValue: []
-                              }
-                          )
+              this.FormStructure = this.CapitalObj(this.FormData)
+              console.log(this.FormStructure)
+
+              this.FormInput.FormId = this.FormStructure.FormId
+              this.FormInput.InputByUser = ''
+              this.FormInput.Timestamp = this.timestamp
+              this.FormInput.Sections = []
+              this.FormStructure.Sections.forEach(
+                  (elementSection, indexSection) => {
+                    this.FormInput.Sections.push(
+                        {
+                          SectionId: this.FormStructure.Sections[indexSection].SectionId,
+                          Components: []
                         }
-                      }
-                  )
-                }
-            )
-          }
-        })
-        .catch(error => {
-          // this.errors.push(error)
-          console.log(error)
-        })
+                    )
+                    this.FormStructure.Sections[indexSection].Components.forEach(
+                        (elementComponent, indexComponent) => {
+                          if (
+                              elementComponent.ComponentType !== 'heading'
+                              && elementComponent.ComponentType !== 'paragraph'
+                          ) {
+                            this.FormInput.Sections[indexSection].Components.push(
+                                {
+                                  ComponentId: this.FormStructure.Sections[indexSection].Components[indexComponent].ComponentId,
+                                  ComponentValue: []
+                                }
+                            )
+                          }
+                        }
+                    )
+                  }
+              )
+            }
+          })
+          .catch(error => {
+            // this.errors.push(error)
+            console.log(error)
+          })
+    }  else {
+    this.$router.push('/form')
+  }
+
+    window.onbeforeunload  = function() {
+      return "Data will be lost if you leave the page, are you sure?"
+    }
   },
   methods: {
+    PrevPage(){
+      this.$router.push('/form')
+    },
+    // Save form
+    doShowSaveModal(){
+      this.StateSaveModal = this.StateSaveModal !== true
+    },
     CapitalFirstLetter(string){
       return string.charAt(0).toUpperCase() + string.slice(1)
     },
@@ -417,4 +460,100 @@ export default {
   }
   .with-line:before,.with-line:after {width: 4rem;}
 }
+
+.theme-modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  z-index: 2;
+}
+.theme-modal {
+  margin: 10rem 0;
+  min-width: 375px;
+  width: 460px;
+  background-color: white;
+  padding: 0.75rem;
+  box-shadow: 0 0 0.5px rgba(10, 10, 10, 0.5);
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  .button-close{
+    color: #102A43;
+    padding: 0.75rem;
+    transition: all .1s ease-in;
+    border-radius: 12px;
+    margin: 0.25rem 0;
+    cursor: pointer;
+    &:hover{
+      color: #D64545;
+      box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+    }
+  }
+}
+.theme-modal-fade-enter, .theme-modal-fade-leave-to {
+  opacity: 0;
+}
+.theme-modal-fade-enter-active, .theme-modal-fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.button__style__white {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: $blue5;
+  padding: 0.75rem;
+  transition: all .1s ease-in;
+  border-radius: 12px;
+  margin: 0.25rem 0;
+  cursor: pointer;
+  border-width: 1px;
+  border-color: $blue5;
+  background-color: white;
+  &:hover{
+    color: $blue3;
+    border-color: $blue3;
+  }
+}
+.button__style__white.red {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: $red5;
+  padding: 0.75rem;
+  transition: all .1s ease-in;
+  border-radius: 12px;
+  margin: 0.25rem 0;
+  cursor: pointer;
+  border-width: 1px;
+  border-color: $red5;
+  background-color: white;
+  &:hover{
+    color: $red3;
+    border-color: $red3;
+  }
+}
+
+.button__style__red {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  padding: 0.75rem;
+  transition: all .1s ease-in;
+  border-radius: 12px;
+  margin: 0.25rem 0;
+  cursor: pointer;
+  background-color: $red5;
+  &:hover{
+    background-color: $red6;
+  }
+}
+
 </style>
